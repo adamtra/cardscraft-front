67358df4518a3b18da8dc4b8e8eaaa29b28d4c4e
Adam Trawiński <adam.trawinski@poczta.fm>
2020-01-08 12:41:53 +0100

    Dołączenie do gry

--- src/app/pages/game/game-manager/game-manager.component.ts
+++ src/app/pages/game/game-manager/game-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { WebSocketService } from 'src/app/services/web-socket.service';
+import { SharedStorage } from 'ngx-store';
 
 @Component({
   selector: 'app-game-manager',
@@ -8,17 +9,22 @@ import { WebSocketService } from 'src/app/services/web-socket.service';
 })
 export class GameManagerComponent implements OnInit {
 
+  @SharedStorage('roomId') roomId: string;
+  
   @HostListener('window:beforeunload')
   disconnect() {
-    this.socket.send('leave', null);
+    this.webSocket.send('leave', this.roomId);
   }
   
   public state = 1;
 
-  constructor(private socket: WebSocketService) { }
+  constructor(private webSocket: WebSocketService) { }
 
   ngOnInit() {
-
+    this.webSocket.connect();
+    this.webSocket.onMessage('start').subscribe(() => {
+      this.state = 2;
+    });
   }
 
 }
